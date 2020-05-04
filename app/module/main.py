14e2914e4e0cad1f2688002fca75d1ca66869074
Fitrianurhayati <fitrianurhayati40@gmail.com>
2020-05-04 22:52:19 +0700

    icon analysisnya belom bisa di klik

--- app/module/main.py
+++ app/module/main.py
@@ -5,3 +5,11 @@ from app import app
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/predict', methods=['GET','POST'])   
+def predict():
+	if request.method =="GET":
+		return render_template("form.html")
+	elif request.method =="POST":
+		return "OK"
