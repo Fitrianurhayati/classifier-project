4a6c7eeb7fdd36ee02cb78089778d92514eb0ec6
Fitrianurhayati <fitrianurhayati40@gmail.com>
2020-05-05 22:56:27 +0700

    upload data nya error

--- app/module/main.py
+++ app/module/main.py
@@ -1,19 +1,23 @@
 from flask import render_template, request
 from app import app
 import os
-
+import pandas as pd 
+from flask import request, render_template
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
 
-@app.route('/predict', methods=['GET','POST'])   
-def predict():
+@app.route('/upload', methods=['GET','POST'])   
+def upload():
 	if request.method =="GET":
 		return render_template("form.html")
 	elif request.method =="POST":
 		file = request.files['file']
 
 		file.save(os.path.join('app/tmp','data_uji.xlsx'))
-		return "OK"
+		df = pd.read_excel("app/tmp/data_uji.xlsx")
+		return render_template("view-data.html", tables=[df.to_html(classes='table table-striped', border=0,index=False, justify='left')])
+
+
